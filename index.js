3339ba8c51500ef9a3d63a09e19390e3c724865e
Thomas AURIOL <thomas.auriol@ynov.com>
2019-12-27 10:54:43 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json")
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('error', console.error);
+
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    const command = msg.content.toLowerCase();
+    if (command === 'ping') {
         msg.reply('Pong!');
     }
 });
 
-client.login('token');
+client.login(config.token);
